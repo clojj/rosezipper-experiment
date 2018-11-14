cbb2590b53b321c734ddddd3371e23ed0bcaf0e4
jwin <jwin1968@gmail.com>
2018-11-14 22:38:42 +0100

    quickcheck added

--- Main.hs
+++ Main.hs
@@ -12,6 +12,7 @@ import           Data.Ord                       ( comparing )
 import           Data.Tree                      ( Tree(Node)
                                                 , rootLabel
                                                 , flatten
+                                                , drawTree
                                                 )
 import           Data.Tree.Zipper               ( TreePos
                                                 , Full
@@ -27,10 +28,13 @@ import           Data.Tree.Zipper               ( TreePos
                                                 , parent
                                                 )
 
+import           Control.Monad.Writer
+import           Data.List
+import qualified Test.QuickCheck               as QC
+import           Test.QuickCheck.Gen
+import           Test.QuickCheck.Arbitrary
+import           Test.QuickCheck.Instances.Containers
 
-import Control.Monad.Writer
-
-import Data.List
 
 testtree :: Tree String
 testtree = Node
@@ -44,10 +48,13 @@ testtree1 :: Tree String
 testtree1 = Node "C" []
 
 testtree2 :: Tree String
-testtree2 = Node "C" [Node "1" [Node "11" []], Node "2" [], Node "3" [Node "33" []]]
+testtree2 =
+  Node "C" [Node "1" [Node "11" []], Node "2" [], Node "3" [Node "33" []]]
 
 testtree3 :: Tree String
-testtree3 = Node "0" [Node "1" [Node "11" [Node "111" []]], Node "2" [Node "22" [Node "222" []]]]
+testtree3 = Node
+  "0"
+  [Node "1" [Node "11" [Node "111" []]], Node "2" [Node "22" [Node "222" []]]]
 
 
 
@@ -56,52 +63,67 @@ data Navigation = Down | Next | Up
 -- traversal in Monad m for each node ...only with cursor-functions 'parent', 'next' and 'firstChild'
 _traverseM :: (Show a, Monad m) => (TreePos Full a -> m b) -> Tree a -> m b
 _traverseM f = go f Down . fromTree
-  where
-    go :: (Show a, Monad m) => (TreePos Full a -> m b) -> Navigation -> TreePos Full a -> m b
-    go fn nav z = 
-      case nav of 
-        Down -> case firstChild z of
-                  Nothing   -> go fn Next z
-                  Just node -> fn node >> go fn Down node
-        Next -> case next z of
-                  Nothing   -> go fn Up z
-                  Just node -> fn node >> go fn Down node
-        Up ->   case parent z of
-                  Nothing   -> fn z
-                  Just node -> go fn Next node
+ where
+  go
+    :: (Show a, Monad m)
+    => (TreePos Full a -> m b)
+    -> Navigation
+    -> TreePos Full a
+    -> m b
+  go fn nav z = case nav of
+    Down -> case firstChild z of
+      Nothing   -> go fn Next z
+      Just node -> fn node >> go fn Down node
+    Next -> case next z of
+      Nothing   -> go fn Up z
+      Just node -> fn node >> go fn Down node
+    Up -> case parent z of
+      Nothing   -> fn z
+      Just node -> go fn Next node
 
 collectNode :: TreePos Full a -> Writer [a] ()
-collectNode z = 
-  let str = label z
-    in
-    tell [str]
+collectNode z = let str = label z in tell [str]
 
 debugNode :: Show a => TreePos Full a -> IO ()
 debugNode = print . label
 
 
-main :: IO ()
-main = 
-  let alltrees = [testtree, testtree1, testtree2, testtree3]
-  in
-  -- IO Monad
-  mapM_ (\t -> print "------------------------------------" >> _traverseM debugNode t) alltrees >>
+-- tests
 
-  -- Writer Monad
-  let result = map (\t -> execWriter $ _traverseM collectNode t) [testtree, testtree1, testtree2, testtree3]
-  in
-    print "-------------------------Writer Monad--------------" >> 
-    mapM_ print result 
-    -- TODO hspec, QuickCheck !
-    >>
-    let alltreesFlattened = map (\t -> flatten t) alltrees
-        zipped = zip alltreesFlattened result
-    in
-      print $ map (\(t, r) -> compList t r) zipped
-
+_prop_traverses_all :: (Eq a, Show a) => Tree a -> QC.Property
+_prop_traverses_all tree =
+  QC.label ("tree size " ++ show (length $ flatten tree))
+    $ compList (flatten tree) (execWriter $ _traverseM collectNode tree)
 
 compList :: (Eq a) => [a] -> [a] -> Bool
 compList x y = null (x \\ y) && null (y \\ x)
+
+
+
+-- main
+
+-- QC.quickCheck $ QC.mapSize ((*) 1000) $ QC.withMaxSuccess 50 _prop_traverses_all
+
+main :: IO ()
+main =
+  let alltrees = [testtree, testtree1, testtree2, testtree3]
+  in
+  -- IO Monad
+    mapM_
+        (\t ->
+          print "------------------------------------" >> _traverseM debugNode t
+        )
+        alltrees
+      >>
+
+  -- Writer Monad
+         let result = map (execWriter . _traverseM collectNode)
+                          [testtree, testtree1, testtree2, testtree3]
+                  in  print "-------------------------Writer Monad--------------"
+                      >> mapM_ print result
+                      >> let alltreesFlattened = map flatten alltrees
+                             zipped            = zip alltreesFlattened result
+                         in  print $ map (\(t, r) -> compList t r) zipped
 
 
 -- other stuff
