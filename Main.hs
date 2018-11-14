0a79920f251e60fd4761c8ef1010c07b90c428be
Joerg Winter <joerg.winter@f-i-solutions-plus.de>
2018-11-14 09:09:39 +0100

    refactor

--- Main.hs
+++ Main.hs
@@ -34,33 +34,38 @@ testtree = Node
   , Node "X" [Node "X1" []]
   ]
 
-testtree' :: Tree String
-testtree' = Node "C" []
+testtree1 :: Tree String
+testtree1 = Node "C" []
 
-testtree'' :: Tree String
-testtree'' = Node "C" [Node "1" [Node "11" []], Node "2" []]
+testtree2 :: Tree String
+testtree2 = Node "C" [Node "1" [Node "11" []], Node "2" []]
+
+testtree3 :: Tree String
+testtree3 = Node "0" [Node "1" [Node "11" [Node "111" []]], Node "2" [Node "22" [Node "222" []]]]
 
 
 main :: IO ()
 main =
-  mapM_ (\t -> print "---" >> _traverse t) [testtree, testtree', testtree'']
+  mapM_ (\t -> print "------------------------------------" >> _traverse t) [testtree, testtree1, testtree2, testtree3]
+
+data Navigation = Down | Next | Up
 
 -- traversal (showing each node) only with cursor-functions 'parent', 'next' and 'firstChild'
 _traverse :: Show a => Tree a -> IO ()
-_traverse = go . fromTree
- where
-  go :: forall a . Show a => TreePos Full a -> IO ()
-  go z = do
-    debugNode z
-    case firstChild z of
-      Nothing -> case next z of
-        Nothing -> case parent z of
-          Nothing -> print "END"
-          Just _parent  -> case next _parent of
-            Nothing         -> print "END next"
-            Just _next -> go _next
-        Just _next -> go _next
-      Just _child -> go _child
+_traverse = go Down . fromTree
+  where
+    go :: forall a . Show a => Navigation -> TreePos Full a -> IO ()
+    go nav z = do
+      case nav of 
+        Down -> case firstChild z of
+                  Nothing   -> go Next z
+                  Just node -> debugNode node >> go Down node
+        Next -> case next z of
+                  Nothing   -> go Up z
+                  Just node -> debugNode node >> go Down node
+        Up ->   case parent z of
+                  Nothing   -> debugNode z
+                  Just node -> go Next node
 
 debugNode :: Show a => TreePos Full a -> IO ()
 debugNode = print . label
